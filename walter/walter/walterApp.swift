7d28af472c93f9b01e3bfe594692d987248a7a30
Heath Borders <heath.borders@gmail.com>
2021-08-28 23:01:28 -0500

    Initial workspace, project

---
+++ walter/walter/walterApp.swift
@@ -0,0 +1,17 @@
+//
+//  walterApp.swift
+//  walter
+//
+//  Created by Heath Borders on 8/28/21.
+//
+
+import SwiftUI
+
+@main
+struct walterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
